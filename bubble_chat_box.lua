c2f679c2123a505ea2a48341fb40a987b3500ace
Karla Andujar <karlaandujar@vt.edu>
2025-10-28 21:36:29 -0400

    Enable bubble chat in the chat window

---
+++ bubble_chat_box.lua
@@ -0,0 +1,5 @@
+local Chat = game:GetService("Chat")
+local function setUpChatWindow()
+		return { BubbleChatEnabled = true }
+end
+Chat:RegisterChatCallback(Enum.ChatCallbackType.OnCreatingChatWindow, setUpChatWindow)
